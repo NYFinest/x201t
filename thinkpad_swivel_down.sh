3bf5bb5e2fdb1e725ed354dde17d9747e59a1c1e
Edwin Flores <edflores@archlinux.us>
2011-05-29 18:01:10 -0400

    automatic swivel scripts

---
+++ thinkpad_swivel_down.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/usr/bin/xrandr -o right 
+xsetwacom set "Serial Wacom Tablet stylus" rotate ccw
+xsetwacom set "Serial Wacom Tablet cursor" rotate ccw
+xsetwacom set "Serial Wacom Tablet eraser" rotate ccw
---
+++ thinkpad_swivel_up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/usr/bin/xrandr -o normal
+xsetwacom set "Serial Wacom Tablet stylus" rotate none
+xsetwacom set "Serial Wacom Tablet cursor" rotate none
+xsetwacom set "Serial Wacom Tablet eraser" rotate none
